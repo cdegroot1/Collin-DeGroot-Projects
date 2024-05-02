9e51f4823741f17d32d8aea0e68d3ab8ea856423
cdegroot1 <168138424+cdegroot1@users.noreply.github.com>
2024-05-01 19:31:28 -0500

    Add files via upload

--- PortfolioV3.sql
+++ PortfolioV3.sql
@@ -129,3 +129,30 @@ where dea.continent is not null
 
 select * 
 from PercentPopulationVaccinated
+
+-- Queries for tableau project
+--1.
+select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as death_percentage
+from PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+--2.
+select location, sum(cast(new_deaths as int)) as total_death_count
+from PortfolioProject..CovidDeaths
+where continent is null
+and location not in ('World','European Union','International')
+group by location
+order by total_death_count desc
+
+--3.
+select Location, Population, max(total_cases) as highest_infection_count, max((total_cases/Population))*100 as percent_population_infected
+from PortfolioProject..CovidDeaths
+group by location, population
+order by percent_population_infected desc
+
+--4.
+select location, population, date, max(total_cases) as highest_infection_count, max((total_cases/population))*100 as percent_population_infected
+from PortfolioProject..CovidDeaths
+group by location,population,date
+order by percent_population_infected desc
